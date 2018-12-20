7e1eefe5ad78ed370078bcbfcf77912cc1a663cd
Jiajun Sun <jiajunstanley@gmail.com>
2018-12-19 22:05:39 -0800

    kick off first training

--- train2.py
+++ train2.py
@@ -19,9 +19,9 @@ from torch.autograd import Variable
 import torch.optim as optim
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--epochs", type=int, default=1, help="number of epochs")
-# parser.add_argument("--image_folder", type=str, default="data/samples", help="path to dataset")
-parser.add_argument("--batch_size", type=int, default=2, help="size of each image batch")
+parser.add_argument("--epochs", type=int, default=10, help="number of epochs")
+parser.add_argument("--image_folder", type=str, default="data/object-dataset/", help="path to dataset")
+parser.add_argument("--batch_size", type=int, default=16, help="size of each image batch")
 parser.add_argument("--model_config_path", type=str, default="config/yolov3.cfg", help="path to model config file")
 # parser.add_argument("--data_config_path", type=str, default="config/coco.data", help="path to data config file")
 # parser.add_argument("--weights_path", type=str, default="weights/yolov3.weights", help="path to weights file")
@@ -29,11 +29,11 @@ parser.add_argument("--class_path", type=str, default="data/udacity.names", help
 # parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
 parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
 parser.add_argument("--n_cpu", type=int, default=0, help="number of cpu threads to use during batch generation")
-# parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
-# parser.add_argument("--checkpoint_interval", type=int, default=1, help="interval between saving model weights")
-# parser.add_argument(
-    # "--checkpoint_dir", type=str, default="checkpoints", help="directory where model checkpoints are saved"
-# )
+parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
+parser.add_argument("--checkpoint_interval", type=int, default=1, help="interval between saving model weights")
+parser.add_argument(
+    "--checkpoint_dir", type=str, default="checkpoints", help="directory where model checkpoints are saved"
+)
 parser.add_argument("--use_cuda", type=bool, default=True, help="whether to use cuda if available")
 opt = parser.parse_args()
 print(opt)
@@ -44,10 +44,6 @@ os.makedirs("output", exist_ok=True)
 os.makedirs("checkpoints", exist_ok=True)
 
 classes = load_classes(opt.class_path)
-
-# Get data configuration
-# data_config = parse_data_config(opt.data_config_path)
-# train_path = data_config["train"]
 
 # Get hyper parameters
 hyperparams = parse_model_config(opt.model_config_path)[0]
@@ -66,7 +62,7 @@ if cuda:
 model.train()
 
 transformed_dataset = CSVDataset(csv_file='data/object-dataset/labels.csv',
-                                           root_dir='data/object-dataset/',
+                                           root_dir=opt.image_folder,
                                            transform=transforms.Compose([
                                                Rescale((416, 416)),
                                                ToTensor()
@@ -115,5 +111,5 @@ for epoch in range(opt.epochs):
 
         model.seen += imgs.size(0)
 
-    # if epoch % opt.checkpoint_interval == 0:
-    #     model.save_weights("%s/%d.weights" % (opt.checkpoint_dir, epoch))
+    if epoch % opt.checkpoint_interval == 0:
+        model.save_weights("%s/%d.weights" % (opt.checkpoint_dir, epoch))
